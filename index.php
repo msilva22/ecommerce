9162a4685d4841626b61ee1fb5bd904c000b3744
Moisés Silva <moises.ambiental@hotmail.com>
2024-02-26 16:05:12 -0300

    Criado a classe Page.

--- index.php
+++ index.php
@@ -2,16 +2,19 @@
 
 require_once("vendor/autoload.php");
 
+use \Slim\Slim;
+use \Hcode\Page;
+
 $app = new \Slim\Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
 
+		$page = new Page();
+
+		$page->setTpl("index");
 		
-		$sql = new Hcode\DB\Sql(); 
-        $results =  $sql->select("select *from tb_users");
-		echo json_encode($results);
 
 });
 
